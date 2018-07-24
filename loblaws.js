7a3052fc3ad1d7c75ca449b863d7d54281c255c1
Eric Hartman <eah458@nyu.edu>
2018-07-24 18:26:18 -0400

    Can assert price 1 > price 2

--- loblaws.js
+++ loblaws.js
@@ -2,7 +2,7 @@ import Page from './page-object';
 import { Selector } from 'testcafe';
 
 fixture `Loblaws`
-    .page `https://www.loblaws.ca/`;
+    .page `https://www.loblaws.ca/search/?search-bar=apples`;
 
 const page       = new Page();
 
@@ -17,12 +17,22 @@ for (var i = 0; i < prices.length; i++) {
 test('Sorting Verification', async t => {
     await t
         // Resize window to fit widest breakpoint (mobile break at 650 and wide break at 960 px)
-        .resizeWindow(1269, 700)
+        // .resizeWindow(1269, 700)
         // Click on search input apples and complete search
-        .click(page.searchBar)
-        .typeText(page.searchBar, 'apples')
-        .pressKey('enter')
+        // .click(page.searchBar)
+        // .typeText(page.searchBar, 'apples')
+        // .pressKey('enter')
         // Click on the sort High-to-low button
-        .click(page.priceSort)
-        .expect(dollarValue1).gte(dollarValue2);
+        .click(page.priceSort);
+
+    
+
+
+    var price1 = page.applePrices1.innerText
+    console.log(await price1)
+    var price2 = page.applePrices2.innerText;
+    console.log(await price2);
+    await t
+        .expect(price1).gte(await price2);
 });
+
