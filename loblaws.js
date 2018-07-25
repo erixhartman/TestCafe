a6cf42ab2e08936830199ff5da6d25c8a2b6f6c0
Eric Hartman <eah458@nyu.edu>
2018-07-24 22:11:59 -0400

    Remove old loop, fix current loop to run x-1 times, make window mobile sized

--- loblaws.js
+++ loblaws.js
@@ -6,20 +6,12 @@ fixture `Loblaws`
 
 const page       = new Page();
 
-var prices = Selector('.reg-price-text');
-
-var priceList = [];
-
-for (var i = 0; i < prices.length; i++) {
-    priceList.push(prices[i].innerText);
-}
-
 test('Sorting Verification', async t => {
     await t
-        // Resize window to fit widest breakpoint (mobile break at 650 and wide break at 960 px)
-        .resizeWindow(1269, 700)
+        // Resize window to fit mobile breakpoint (mobile break at 650 and wide break at 960 px)
+        .resizeWindow(649, 700)
         // Uncomment line below to Click Language Button to trigger french
-        .click(page.languageButton)
+        // .click(page.languageButton)
         // Click on search input apples and complete search
         .click(page.searchBar)
         .typeText(page.searchBar, 'apples')
@@ -28,16 +20,18 @@ test('Sorting Verification', async t => {
         .click(page.priceSort);
 
     
+    // // Set variables to get the number of items listed
     var prices = page.applePrices
     var count    = await prices.count;
 
-    for (var i = 0; i < count; i++) {
+    // Loop all items and compare to the one following it
+    for (var i = 0; i < (count-1); i++) {
         var price1 = page.applePrices.nth(i).innerText
         console.log(await price1)
-        var price2 = page.applePrices.nth(i+1).innerText;
+        var price2 = page.applePrices.nth(i+1).innerText
         console.log(await price2);
-        await t
-            .expect(price1).gte(await price2);
+    await t
+       .expect(price1).gte(await price2);
     }
 
 });
